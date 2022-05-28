6b83afed3f2c7ea68311e67e57012214530050f1
alex <alexej.kisselev@gmail.com>
2022-05-28 09:18:32 +0200

    add translation

--- src/Controller/Admin/AbstractAppGrudController.php
+++ src/Controller/Admin/AbstractAppGrudController.php
@@ -25,14 +25,17 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class AbstractAppGrudController extends AbstractCrudController
 {
     private EntityManagerInterface $em;
+    private TranslatorInterface $translator;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, TranslatorInterface $translator)
     {
         $this->em = $em;
+        $this->translator = $translator;
     }
 
     /**
@@ -55,46 +58,47 @@ abstract class AbstractAppGrudController extends AbstractCrudController
         $classMetadata = $this->getEntityManager()->getClassMetadata(static::getEntityFqcn());
         $mappings = array_replace_recursive($classMetadata->fieldMappings, $classMetadata->associationMappings);
         foreach ($mappings as $propertyName => $mapping) {
+            $label = $this->getTranslator()->trans(ucfirst($propertyName));
             switch ($mapping['type']) {
                 case 'integer':
                     if ($propertyName !== 'id') {
-                        $fields[$propertyName] = IntegerField::new($propertyName, ucfirst($propertyName));
+                        $fields[$propertyName] = IntegerField::new($propertyName, $label);
                     } else {
-                        $fields[$propertyName] = IdField::new($propertyName, ucfirst($propertyName))->hideOnForm();
+                        $fields[$propertyName] = IdField::new($propertyName, $label)->hideOnForm();
                     }
                     break;
                 case 'float':
                 case 'decimal':
-                    $fields[$propertyName] = NumberField::new($propertyName, ucfirst($propertyName));
+                    $fields[$propertyName] = NumberField::new($propertyName, $label);
                     break;
                 case 'boolean':
-                    $fields[$propertyName] = BooleanField::new($propertyName, ucfirst($propertyName));
+                    $fields[$propertyName] = BooleanField::new($propertyName, $label);
                     break;
                 case 'datetime':
                 case 'date_immutable':
-                    $fields[$propertyName] = DateTimeField::new($propertyName, ucfirst($propertyName))->setFormat('y-MM-d hh:mm:ss')->hideOnForm();
+                    $fields[$propertyName] = DateTimeField::new($propertyName, $label)->setFormat('y-MM-d hh:mm:ss')->hideOnForm();
                     break;
                 case 'date':
-                    $fields[$propertyName] = DateField::new($propertyName, ucfirst($propertyName))->setFormat('y-MM-d hh:mm:ss')->hideOnForm();
+                    $fields[$propertyName] = DateField::new($propertyName, $label)->setFormat('y-MM-d hh:mm:ss')->hideOnForm();
                     break;
                 case 'json':
                 case 'array':
                 case 'array_simple':
-                    $fields[$propertyName] = ArrayField::new($propertyName, ucfirst($propertyName));
+                    $fields[$propertyName] = ArrayField::new($propertyName, $label);
                     break;
                 case ClassMetadataInfo::MANY_TO_MANY:
                 case ClassMetadataInfo::ONE_TO_MANY:
                 case ClassMetadataInfo::MANY_TO_ONE:
-                    $fields[$propertyName] = AssociationField::new($propertyName, ucfirst($propertyName));
+                    $fields[$propertyName] = AssociationField::new($propertyName, $label);
                     break;
                 default:
                     if ($propertyName === 'password') {
                         continue 2;
                     }
                     if ($propertyName === 'email') {
-                        $fields[$propertyName] = EmailField::new($propertyName, ucfirst($propertyName));
+                        $fields[$propertyName] = EmailField::new($propertyName, $label);
                     } else {
-                        $fields[$propertyName] = TextField::new($propertyName, ucfirst($propertyName));
+                        $fields[$propertyName] = TextField::new($propertyName, $label);
                     }
             }
         }
@@ -124,6 +128,14 @@ abstract class AbstractAppGrudController extends AbstractCrudController
         return $fields;
     }
 
+    /**
+     * @return TranslatorInterface
+     */
+    public function getTranslator(): TranslatorInterface
+    {
+        return $this->translator;
+    }
+    
     protected function getEntityManager(): EntityManagerInterface
     {
         return $this->em;
